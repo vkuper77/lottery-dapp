87df586d76f3c8c7ed0b418574274299b2d907c1
Vitali <kupratsevich@msa-it.ru>
2023-02-19 00:19:17 +0300

    feat: add config

---
+++ truffle-config.js
@@ -0,0 +1,24 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const keys = require('./keys.json')
+
+module.exports = {
+  contracts_build_directory: "./public/contracts",
+  networks: {
+    // inf_WhiteList_goerli: {
+    //   network_id: 5,
+    //   gasPrice: 80000000000,
+    //   provider: new HDWalletProvider(keys.MNEMONIC, `https://goerli.infura.io/v3/${}`)
+    // },
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*",
+    }
+  },
+  mocha: {},
+  compilers: {
+    solc: {
+      version: "0.8.17"
+    }
+  }
+};
