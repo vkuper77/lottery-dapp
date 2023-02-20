1c63cd88cc74ec9eee281fa5855d7912fd7512fa
Vitali <kupratsevich@msa-it.ru>
2023-02-20 09:58:36 +0300

    refactor: app

--- components/UserCard.js
+++ components/UserCard.js
@@ -1,13 +1,12 @@
 import style from '../styles/Header.module.css'
 import truncateEthAddress from 'truncate-eth-address'
-// TODO: Destructure the prop we passed from Header.js
+
 const UserCard = ({address}) => {
   return (
     <div>
       <div className={`${style.welcome} ${style.loginBtn}`}>
         👋 Welcome,{' '}
         <span className={style.accentColor}>
-          {/* Render the users address */}
           {truncateEthAddress(address)}
         </span>
       </div>
